bf33b1db1f459ce1619acdaf476b4acecc1c2a4a
Viktar <gxvint@gmail.com>
2017-04-03 16:45:52 +0300

    Init from rr-starter-kit

---
+++ client/app.jsx
@@ -0,0 +1,14 @@
+import 'index.html';
+import 'common/styl/global.styl';
+
+import React from 'react';
+import AppContainer from 'appContainer';
+import {render} from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+window.startApp = () => render(<AppContainer/>, document.getElementById('app'));
+/^(http|https):\/\//.test(document.URL) ?
+    window.startApp() :
+    document.addEventListener('deviceready', window.startApp, false);
---
+++ tests/e2e/pageObjects/masterPage.js
@@ -0,0 +1,6 @@
+export class MasterPage {
+
+    //Elements
+    header = "header";
+    url = '/';
+}
